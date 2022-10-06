d61cd0e401c561822859222d0ceb23604ac2b0d7
Ghazala Ahmed <ghazalaahmed@ghazalas-air.lan>
2022-10-05 23:53:25 -0700

    changes

--- script.js
+++ script.js
@@ -1,56 +1,83 @@
 // Assignment code here
-var generateBtn = document.getElementById("generate");
+var generateBtn = document.querySelector("#generate");
+
+function randomInt(min, max) {
+if (max) {
+max = min
+min = 0
+}
+var rand = Math.random()
+return Math.floor(min*(1 - rand) + rand*max)
+}
+
+function getRandomItem(list) {
+return list[randomInt(0, list.length - 1)]
+}
 
 function generatePassword() {
 
-var specialCharacters = "!@%#&*+=^";
-var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numericCharacters = "0123456789";
-var passwordLength = 8
+var userInput = window.prompt("How long do you want your password to be?")
 
-var hasSpecChars = confirm("Do you want to include specialCharacters?")
-var hasLwercaseLetters = confirm("Do you want to include lowerCaseNumbers?")
-var hasUppercaseLetters = confirm("Do you want to include upperCaseLetters?")
-var hasNumericCharacters = confirm("Do you want to include numericCharacters?")
+var passwordLength = parseInt(userinput)
+ 
 
-if (passwordLength < 8 || passwordLength > 128) {
-  Window.alert("password length must be at least 8 and no greater than 128 characters")
+if(isNaN(passwordLength)) {
+  window.alert("That's not a number!")
   return
-}
-
-if(isNotANumericCharacter(passwordLength)) {
-  window.alert("That's not a number")
-  return
-}
-
-if (isNotANumericCahracter(passwordLength)) {
-window.alert("That's not a number")
-} else if (passwordLength < 8|| passwordLength > 128) {
-  window.alert("invalid passord length")
-} else {
-  return
-}
-
-  var password = generatePassword();
-
-  var optionsCart = []
-
-for (var i=0; i< lowerCaseLetters.length; i++) 
-    uppercaseLetters[i] = lowercaseLetters[i].touppercaseLetters() 
-    Math.floor(Math.random() * characters.length);
   }
 
-  
-//write password to the #password input
-function writePassword() {
-  var password=generatePassword();
-  var passwordTxt = document.getElementById("password");
-
-  passwordTxt.value = password;
-
+if(passwordLength < 8 || passwordLength > 128) {
+window.alert("The length of password must be between 8 and 128 characters")
+return
 }
 
+var SpecialCharacters = window.confirm("Do you want to include specialCharacters?")
+var hasLwercaseLetters = window.confirm("Do you want to include lowerCaseNumbers?")
+var hasUppercaseLetters = window.confirm("Do you want to include upperCaseLetters?")
+var hasNumericCharacters = window.confirm("Do you want to include numericCharacters?")
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+var specialCharacters = ["!","@","%","#","&","*","+"]
+var lowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x", "y","z"]
+var uppercaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var numericCharacters = ["0","1","2","3","4","5","6","7","8","9"]
+
+var options = []
+
+for (var i = 0; i< lowercaseLetters.length; i++) {
+  uppercaseLetters[i] = lowercaseLetters[i].toUpperCase() 
+}
+  
+if(numericCharacters === true) 
+  options.push(numericCharacters)
+  }
+
+if(specialCharacters === true) {
+  options.push(specialCharacters)
+  }
+
+if(lowercaseLetters === true) {
+  options.push(lowercaseLetters)
+  }
+
+if(uppercaseLetters === true) {
+  options.push(uppercaseLetters)
+  }
+
+  var generatePassword = ""
+
+  for (var i = 0; i < password.Length; i++) {
+  var randomItem = options[randomInt(0, options.length - 1)]
+  }
+
+//Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  
+  }
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+  
